1460bdde5ecac5d5ea1a0570cee4523fd0f46c26
kcb0126 <kcb0126@outlook.com>
2018-03-28 03:44:20 +0800

    Add a github project "Cleverscape.UTorrentClient.WebClient"

---
+++ TorrLogger/Managers/TorrentsManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TorrLogger.Managers
+{
+    class TorrentsManager
+    {
+        public static TorrentsManager Instance
+        {
+            get
+            {
+                if(_instance == null)
+                {
+                    _instance = new TorrentsManager();
+                }
+                return _instance;
+            }
+        }
+        private static TorrentsManager _instance = null;
+
+        public TorrentsManager()
+        {
+
+        }
+    }
+}
