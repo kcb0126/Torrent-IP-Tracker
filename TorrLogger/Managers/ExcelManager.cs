04db603710ea40b170a3a30795c9be4ef19a48a6
kcb0126 <kcb0126@outlook.com>
2018-04-27 01:12:31 +0800

    Reduce intervals for refreshing and filtered self ip.

---
+++ TorrLogger/Managers/ExcelManager.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TorrLogger.ViewModels;
+
+namespace TorrLogger.Managers
+{
+    class ExcelManager
+    {
+        public static ExcelManager Instance
+        {
+            get
+            {
+                if(_instance == null)
+                {
+                    _instance = new ExcelManager();
+                }
+                return _instance;
+            }
+        }
+
+        public void SaveClientViewModels(ObservableCollection<ClientViewModel> models, string excelFileName)
+        {
+            Microsoft.Office.Interop.Excel.Application oXL;
+            Microsoft.Office.Interop.Excel._Workbook oWB;
+            Microsoft.Office.Interop.Excel._Worksheet oSheet;
+            Microsoft.Office.Interop.Excel.Range oRng;
+            object misValue = System.Reflection.Missing.Value;
+            try
+            {
+                //Start Excel and get Application object.
+                oXL = new Microsoft.Office.Interop.Excel.Application();
+                oXL.Visible = false;
+
+                //Get a new workbook.
+                oWB = (Microsoft.Office.Interop.Excel._Workbook)(oXL.Workbooks.Add(""));
+                oSheet = (Microsoft.Office.Interop.Excel._Worksheet)oWB.ActiveSheet;
+
+                //Add table headers going cell by cell.
+                oSheet.Cells[1, 1] = "No";
+                oSheet.Cells[1, 2] = "IP Address";
+                oSheet.Cells[1, 3] = "Port";
+                oSheet.Cells[1, 4] = "Data";
+                oSheet.Cells[1, 5] = "Time";
+                oSheet.Cells[1, 6] = "Title";
+                oSheet.Cells[1, 7] = "File Hash";
+                oSheet.Cells[1, 8] = "Country";
+                oSheet.Cells[1, 9] = "ISP";
+
+                //Format A1:F1 as bold, vertical alignment = center.
+                oSheet.get_Range("A1", "I1").Font.Bold = true;
+                oSheet.get_Range("A1", "I1").VerticalAlignment = Microsoft.Office.Interop.Excel.XlVAlign.xlVAlignCenter;
+
+                for(int i = 0; i < models.Count; i++)
+                {
+                    oSheet.Cells[i + 2, 1] = models[i].No;
+                    oSheet.Cells[i + 2, 2] = models[i].IpAddress;
+                    oSheet.Cells[i + 2, 3] = models[i].Port;
+                    oSheet.Cells[i + 2, 4] = models[i].Date;
+                    oSheet.Cells[i + 2, 5] = models[i].Time;
+                    oSheet.Cells[i + 2, 6] = models[i].Title;
+                    oSheet.Cells[i + 2, 7] = models[i].FileHash;
+                    oSheet.Cells[i + 2, 8] = models[i].Country;
+                    oSheet.Cells[i + 2, 9] = models[i].ISP;
+                }
+
+                //AutoFit columns A:H.
+                oRng = oSheet.UsedRange;
+                oRng.EntireColumn.AutoFit();
+
+                oXL.Visible = false;
+                oXL.UserControl = false;
+                oWB.SaveAs(excelFileName, Microsoft.Office.Interop.Excel.XlFileFormat.xlWorkbookDefault, Type.Missing, Type.Missing,
+                    false, false, Microsoft.Office.Interop.Excel.XlSaveAsAccessMode.xlNoChange,
+                    Type.Missing, Type.Missing, Type.Missing, Type.Missing, Type.Missing);
+
+                oWB.Close();
+            }
+            catch
+            {
+                int notused = 0;
+            }
+        }
+
+        private static ExcelManager _instance = null;
+    }
+}
--- TorrLogger/Utils/Utils.cs
+++ TorrLogger/Utils/Utils.cs
@@ -36,8 +36,30 @@ namespace TorrLogger.Utils
             return string.Format("{0:0.##} {1}", size, sizeUnit);
         }
 
+        private static string _myip = string.Empty;
+
+        private static string MyIp() {
+            if (_myip.Equals(string.Empty))
+            {
+                dynamic result = IspAndCountryFromIp(string.Empty);
+                _myip = result.Query;
+            }
+            return _myip;
+        }
+
         public static ExpandoObject IspAndCountryFromIp(string ip)
         {
+            if (!ip.Equals(string.Empty))
+            {
+                if (ip.Equals(MyIp()))
+                {
+                    dynamic result1 = new ExpandoObject();
+                    result1.Country = "Unknown";
+                    result1.Isp = "Unknown";
+                    return result1;
+                }
+            }
+
             var json = new WebClient().DownloadString("http://ip-api.com/json/" + ip);
             dynamic jsonObj;
             dynamic result = new ExpandoObject();
@@ -53,6 +75,7 @@ namespace TorrLogger.Utils
                 }
                 result.Country = jsonObj.country;
                 result.Isp = jsonObj.isp;
+                result.Query = jsonObj.query;
                 return result;
             }
             catch
